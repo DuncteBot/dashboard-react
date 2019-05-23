bbe438c76f83e0a2e5b99fa72b82a1faec0397fe
Duncan Sterken <duncan.sterken123@gmail.com>
2019-05-23 09:34:06 +0200

    Move materialize imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import 'materialize-css/sass/materialize.scss';
-import 'materialize-css';
 import Loader from "./components/loader";
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'materialize-css/sass/materialize.scss';
+import 'materialize-css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
