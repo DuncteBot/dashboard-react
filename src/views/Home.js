7bd162b8d9113ee57ceadfa819cd10d4badf5dd4
Duncan Sterken <duncan.sterken123@gmail.com>
2019-06-16 16:06:39 +0200

    Hide login url when no url is ready

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,13 +4,18 @@ import {Link} from 'react-router-dom';
 export default class Home extends Component {
 
     render() {
+        const {loggedIn, url} = this.props.state;
+
         return (
             <div>
                 <div className="container">
                     <div className="row">
                         <div className="col s12 center">
-                            {this.props.state.loggedIn ? <h1>Hi here, <Link to="/guilds">Click me</Link></h1> :
-                                <h1><a href={this.props.state.url}>Log in</a></h1>}
+                            {loggedIn ? <h1>Hi here, <Link to="/guilds">Click me</Link></h1> :
+                                <div>
+                                    {url ? <h1><a href={this.props.state.url}>Log in</a></h1> : 'Loading'}
+                                </div>
+                            }
                         </div>
                     </div>
                 </div>
